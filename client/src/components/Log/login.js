51d1a2a6665bef312605eba18300fc6c40a56d08
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-04-01 23:30:50 +0100

    le style de merde ;(
    (en manque d'inspi)

--- client/src/components/Log/login.js
+++ client/src/components/Log/login.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const login = () => {
     return (
-        <div className='box-register'>
-            <input type="email" placeholder="Entrez votre adresse mail"/>
-            <input type="password" placeholder="Entrez votre mot de passe"/>
+        <div className='box-login form'>
+            <h1>Login</h1>
+            <div>
+                <label htmlFor='email'>Adresse mail:</label><br/>
+                <input type="email" id="email" placeholder="Entrez votre adresse mail"/>
+            </div>
+            <div>
+                <label htmlFor='password'>Mot de passe:</label><br/>
+                <input type="password" id="password" placeholder="Entrez votre mot de passe"/>
+            </div>
             <input type="submit"/>
         </div>
     );
--- client/src/components/Log/register.js
+++ client/src/components/Log/register.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 const register = () => {
     return (
-        <div className='box-login'>
-            <input type="email" placeholder="Entrez votre adresse mail"/>
-            <input type="password" placeholder="Entrez votre mot de passe"/>
-            <input type="password" placeholder="Répétez votre mot de passe"/>
+        <div className='box-register form'>
+            <h1>Register</h1>
+            <div>
+                <label htmlFor='email'>Adresse mail:</label><br/>
+                <input type="email" id='email' placeholder="Entrez votre adresse mail"/>
+            </div>
+
+            <div>
+                <label htmlFor='password'>Mot de passe:</label><br/>
+                <input type="password" id='password' placeholder="Entrez votre mot de passe"/>
+            </div>
+            <div>
+                <label htmlFor='password'>Mot de passe:</label><br/>
+                <input type="password" placeholder="Répétez votre mot de passe"/>
+            </div>
             <input type="submit"/>
         </div>
     );
